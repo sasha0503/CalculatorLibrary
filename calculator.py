0db73aeb148126bc808713e9b644837b96ba1290
oleksandr <o.l@smoothr.de>
2022-07-20 16:30:00 +0300

    implemented division

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def divide(first_term, second_term):
+    return first_term / second_term
