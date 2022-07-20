f7ce9394d29edea81746c288b85d9f4c96cebab5
oleksandr <o.l@smoothr.de>
2022-07-20 17:02:58 +0300

    use unittest instead of pytest

--- test_calculator.py
+++ test_calculator.py
@@ -3,15 +3,19 @@ Unit tests for the calculator library
 """
 
 import calculator
+import unittest
 
 
-class TestCalculator:
+class TestCalculator(unittest.TestCase):
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        self.assertTrue(4 == calculator.add(2, 2))
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        self.assertTrue(2 == calculator.subtract(4, 2))
 
     def test_division(self):
-        assert 2 == calculator.divide(4, 2)
+        self.assertTrue(2 == calculator.divide(4, 2))
 
+
+if __name__ == '__main__':
+    unittest.main()
